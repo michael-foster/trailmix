ab40195fe36699d11f69a84260d85ccf64b1ad95
Michael Dominic Foster <mfoster@outsideinc.com>
2022-02-02 19:38:20 -0700

    Ignore asset docs for now

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,26 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   stories: [
-    "../01-atoms/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../01-atoms/**/*.docs.@(js|jsx|ts|tsx|mdx)",
+    '../components/01-atoms/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../components/01-atoms/**/*.docs.@(js|jsx|ts|tsx|mdx)'
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-controls",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls'
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
     });
 
     config.resolve.modules = [
       ...(config.resolve.modules || []),
-      path.resolve(__dirname, "../"),
+      path.resolve(__dirname, '../')
     ];
 
     return config;
-  },
+  }
 };
